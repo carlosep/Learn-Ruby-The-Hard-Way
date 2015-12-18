5b1ee4d88b7481fc641dd38b5550ed8b38ef9cf5
Carlos E. Palha <carlao_epalha@hotmail.com>
2015-12-18 10:41:20 -0200

    Adding Comment

--- ex13.rb
+++ ex13.rb
@@ -1,5 +1,5 @@
 first, second, third = ARGV #ARGV = argument variable. pass arguments when you run the script.
-ARGV.clear
+ARGV.clear #clears the ARGV before using gets.chomp so gets.chomp returns the line from the standard input instead.
 
 puts "shit... i forgot a needed four arguments..."
 puts "whatever dude, just pass one more."
