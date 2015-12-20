d6eb5e8f9ac1fd8004e067c97ebddf3d015f7394
Carlos E. Palha <carlao_epalha@hotmail.com>
2015-12-20 11:21:24 -0200

    Solving drill

--- ex29.rb
+++ ex29.rb
@@ -31,3 +31,106 @@ end
 if people == dogs
 	puts "People are dogs"
 end
+
+
+#Can you put other boolean expressions from Exercise 27 in the if-statement? Try it.
+if(true && true)
+	puts true
+else 
+	puts false
+end
+if(false && true)
+	puts true
+else 
+	puts false
+end
+if(1 == 1 && 2 == 1)
+	puts true
+else 
+	puts false
+end
+if("test" == "test")
+	puts true
+else 
+	puts false
+end
+if(1 == 1 || 2 != 1)
+	puts true
+else 
+	puts false
+end
+if(true && 1 == 1)
+	puts true
+else 
+	puts false
+end
+if(false && 0 != 0)
+	puts true
+else 
+	puts false
+end
+if(true || 1 == 1)
+	puts true
+else 
+	puts false
+end
+if("test" == "testing")
+	puts true
+else 
+	puts false
+end
+if(1 != 0 && 2 == 1)
+	puts true
+else 
+	puts false
+end
+if("test" != "testing")
+	puts true
+else 
+	puts false
+end
+if("test" == 1)
+	puts true
+else 
+	puts false
+end
+if(!(true && false))
+	puts true
+else 
+	puts false
+end
+if(!(1 == 1 && 0 != 1))
+	puts true
+else 
+	puts false
+end
+if(!(10 == 1 || 1000 == 1000))
+	puts true
+else 
+	puts false
+end
+if(!(1 != 10 || 3 == 4))
+	puts true
+else 
+	puts false
+end
+if(!("testing" == "testing" && "Zed" == "Cool Guy"))
+	puts true
+else 
+	puts false
+end
+if(1 == 1 && (!("testing" == 1 || 1 == 0)))
+	puts true
+else 
+	puts false
+end
+if("chunky" == "bacon" && (!(3 == 4 || 3 == 3)))
+	puts true
+else 
+	puts false
+end
+if(3 == 3 && (!("testing" == "testing" || "Ruby" == "Fun")))
+	puts true
+else 
+	puts false
+end
